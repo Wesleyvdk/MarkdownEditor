6f6218a1b46e051256bedca7a61491e4bcb40639
wesleyvdk <shinxisrl@outlook.com>
2025-09-19 17:35:12 +0200

    update: remove old architecture and include a new structured client/server architecture. this includes starting a server/backend architecture with fastify

--- app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("api/notes", "routes/api.notes.tsx"),
-  route("api/notes/:id", "routes/api.notes.$id.tsx"),
-] satisfies RouteConfig;
